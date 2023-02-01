d2b39f7fd4f65597888434520c5a560982064515
GhimireMadan <111294836+GhimireMadan@users.noreply.github.com>
2023-02-01 10:41:17 -0500

    patch

--- FP-Initial/Choose_Experiment_FP.m
+++ FP-Initial/Choose_Experiment_FP.m
@@ -4,11 +4,18 @@ clear all
 % first make a variable with path to your spreadsheet containing your
 % experimental information
 addpath('./utils/')
+
+% These are the example inputs, only change the values in the below 5 lines
+% to the desired experimental setup. Currently, this code is set up to run
+% FRA and SAM noise parameters. Future patch is necessary to run further
+% parameters
 input.AnimalID = '585A';
 input.Date_of_experiment = [10923, 11023, 11323, 11723, 11723, 11823, 11923, 12723, 13023];
 input.spreadsheet = 'W:\Data\Fiber_Photometry\DataPath';
+Batch_data = [];
+input.data_window = [-1 4];
 
-%
+%% selects animals based on the above mentioned criteria
 input.data_matrix = readtable(input.spreadsheet, 'TextType', 'char','VariableNamingRule','preserve');
 input.Animal = find(strcmp(input.data_matrix.("Animal ID"), input.AnimalID) == 1);
 
@@ -19,8 +26,8 @@ input.Date_idx = find(input.Date);
 
 input.data_spreadsheet = input.data_matrix(intersect(input.Animal, input.Date_idx), :);
 
-Batch_data = [];
-input.data_window = [-1 4];
+% Preprocess the selected data and alligns with the stimulus files and
+% generates a summary data
 
 for i = 1:numel(input.data_spreadsheet(:,1))
     date(i,1) = input.Date_of_experiment(i);
